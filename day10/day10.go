263321b0cf42d18c261a4b9a383d150c9e23a485
UnikMask <alexandre.guerville@hotmail.com>
2021-12-12 15:08:59 +0000

    Made day 10 shorter.

--- day10/day10.go
+++ day10/day10.go
@@ -10,34 +10,7 @@ import (
 var chunkOpens = []rune{'{', '[', '(', '<'}
 var chunkCloses = map[rune]rune{'{': '}', '[': ']', '(': ')', '<': '>'}
 var scoreMap = map[rune]int{'}': 1197, ')': 3, ']': 57, '>': 25137}
-var completionMap = map[rune]int{'}': 3, ')': 1, ']': 2, '>': 4}
-
-type ListNode struct {
-	char rune
-	next *ListNode
-}
-
-type stack struct {
-	first *ListNode
-}
-
-func (s *stack) push(c rune) {
-	node := &ListNode{char: c}
-	if s.first != nil {
-		node.next = s.first
-	}
-	s.first = node
-}
-
-func (s *stack) pop() rune {
-	if s.first != nil {
-		popped := s.first
-		s.first = s.first.next
-		return popped.char
-	} else {
-		return '\n'
-	}
-}
+var completionMap = map[rune]int{'}': 3, ')': 1, ']': 2, '>': 4, '\n': 0}
 
 func main() {
 	f, _ := os.Open("part1.data")
@@ -46,16 +19,15 @@ func main() {
 	completions := []int{}
 file:
 	for scanner.Scan() {
-		errorStack := stack{}
+		errorStack := []rune{0}
 	line:
 		for _, char := range scanner.Text() {
-			pop := errorStack.pop()
-			for _, i := range append(chunkOpens, pop) {
-				if char == i {
-					if i != pop {
-						errorStack.push(pop)
-						errorStack.push(chunkCloses[i])
-					}
+			for _, i := range chunkOpens {
+				if char == errorStack[0] {
+					errorStack = errorStack[1:]
+					continue line
+				} else if i == char {
+					errorStack = append([]rune{chunkCloses[i]}, errorStack...)
 					continue line
 				}
 			}
@@ -63,13 +35,12 @@ file:
 			continue file
 		}
 		lineCompletion := 0
-		for next := errorStack.pop(); next != '\n'; next = errorStack.pop() {
+		for _, next := range errorStack {
 			lineCompletion = lineCompletion*5 + completionMap[next]
 		}
 		completions = append(completions, lineCompletion)
 	}
-	fmt.Printf("Syntax Error Score: %d\n", syntaxScore)
-
 	sort.Ints(completions)
+	fmt.Printf("Syntax Error Score: %d\n", syntaxScore)
 	fmt.Printf("Completion Score: %d\n", completions[len(completions)/2])
 }
